5d0694f048da3f4fa56bd4b1745b1732184fd88f
Karina Borges <karina_borges@id.uff.br>
2022-06-22 20:46:59 -0300

    💄 add hover behavior on Lesson card

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -18,9 +18,10 @@ const Lesson = ({ title, slug, availableAt, type }: LessonProps) => {
   );
 
   return (
-    <a href="#">
+    <a href="#" className="group">
       <span className="text-gray-300">{availableAtFormatted}</span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex itens-center gap-2">
